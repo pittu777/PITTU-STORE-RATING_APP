90e62118b14db7d0de1d650dccaf972e430ad602
pittu777 <pittuprasanth98@gmail.com>
2025-07-26 17:44:28 +0530

    "fixed cors issue "

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ const app = express();
 
 app.use(express.json());
 // app.use(cors({origin:"http://localhost:5173", credentials:true}));
-app.use(cors({origin:"https://pittu-store-rating-app-git-master-pittu777s-projects.vercel.app", credentials:true}));
+app.use(cors({origin:"https://pittu-store-rating-app-git-master-pittu777s-projects.vercel.app", credentials:true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
 
